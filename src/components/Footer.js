fe15490f826fdda5abe56589bb1773be7beda7ae
pao1984 <paospina436@misena.edu.co>
2021-10-30 20:44:43 -0500

    pao

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div className = 'let'>
+      Creado por <a href="https://www.facebook.com/paola.ospinrestrepo/">PAOLA OSPINA </a>
+    </div>
+    <div>
+      <a href="https://www.facebook.com/paola.ospinrestrepo/">Sigueme en Instagram</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
